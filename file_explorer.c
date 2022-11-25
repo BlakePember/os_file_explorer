0ca56b6819b50c10e99b47297a373b4e64922553
BlakePember <76570994+BlakePember@users.noreply.github.com>
2022-11-25 16:11:20 -0500

    Update file_explorer.c

--- file_explorer.c
+++ file_explorer.c
@@ -25,7 +25,7 @@ int main(){
     while(1){
         printf("Starting in the C drive\n");
         dir = opendir("C:\\");
-        printf("Input a command\n0 for current directory\n1 for ls\n2 for cd\n3 to delete \n4 to create a folder\n5 to exit\n\n");
+        printf("Input a command\n0 for current directory\n1 for ls\n2 for cd\n3 to delete file\n4 to create a folder\n5 cp a file\n6 to exit\n\n");
         scanf("%d",&input);
         switch(input){
             case 0: 
@@ -49,7 +49,7 @@ int main(){
                 dir = opendir(current);
                 break;
             case 3: 
-                printf("enter name of folder you wnat to remove\n");
+                printf("enter name of folder you want to remove\n");
                 scanf("%s", rm_file);
                 # use a for loop to check to see if the folder already exists
                 # if it doesnt then print an error message and break
@@ -63,11 +63,24 @@ int main(){
                 # else create the folder
                 break;
             case 5:
+                printf("enter name of folder you want to copy\n");
+                scanf("%s", initial_file);
+                # use a for loop to check to see if the folder already exists
+                # if it doesnt then print an error message and break
+                # else continue
+                printf("enter name of folder you want to copy to\n");
+                scanf("%s", new_file);
+                # use a for loop to check to see if the folder already exists
+                # if it does then print an error message and break
+                # else create the folder
+                # then copy contents of file
+                break;
+            case 6:
                 printf("exiting file explorer\n");
                 return 0;
             default:
                 printf("Did not enter an acceptable command\n");
-                break
+                break;
         }
     }
     //should not get here

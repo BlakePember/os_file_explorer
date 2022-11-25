493f771c64d3624eb7ad12cb49dd8b0480607771
Vywren <119077371+Vywren@users.noreply.github.com>
2022-11-25 14:54:33 -0500

    Add files via upload

---
+++ file_explorer.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <dirent.h>
+
+int main(){
+    int input;
+    int end = 0;
+    char current[] = "c\\:";
+    char go_to[] = "";
+    DIR* dir;
+    struct dirent* entity;
+    //for(int i = 0; i< 25; i++){
+        //current[0] += i;
+        //printf("%s\n",current);
+        dir = opendir("c:\\");
+        entity = readdir(dir);
+        while(entity != NULL){
+            printf("%s\n",entity->d_name);
+            entity = readdir(dir);
+    }
+        
+        //}
+    while(1){
+        printf("Input a command\n0 for current directory\n1 for ls\n2 for cd\n3 to delete \n4 to create a folder\n5 to exit\n\n");
+        scanf("%d",&input);
+        switch(input){
+            case 0: 
+                printf("Current directory is %s\n", current);
+                break;
+            case 1:
+                printf("Displaying contents of folder\n");
+                end = 1;
+                break;
+            case 2:
+                printf("enter name of folder\n");
+                scanf("%d", go_to);
+                break;
+            case 5:
+                printf("exiting file explorer\n");
+                closedir(dir);
+                return 0;
+        }
+    }
+    //should not get here
+   return 0;
+}

a7d8328c76132c9adc94421480ee029c97f3e5fa
Blake Pember <blakeapember@gmail.com>
2022-11-28 20:23:02 -0500

    commented function out

--- file_explorer.c
+++ file_explorer.c
@@ -1,11 +1,35 @@
 #include <stdio.h>
 #include <dirent.h>
 #include <string.h>
-#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 #define BUF 20
+/*void listFiles(const char* dirname) {
+    DIR* dir = opendir(dirname);
+    if (dir == NULL) {
+        return;
+    }
+
+    printf("Reading files in: %s\n", dirname);
+
+    struct dirent* entity;
+    entity = readdir(dir);
+    while (entity != NULL) {
+        printf("%hhd %s/%s\n", entity->d_type, dirname, entity->d_name);
+        if (entity->d_type == DT_DIR && strcmp(entity->d_name, ".") != 0 && strcmp(entity->d_name, "..") != 0) {
+            char path[100] = { 0 };
+            strcat(path, dirname);
+            strcat(path, "/");
+            strcat(path, entity->d_name);
+            listFiles(path);
+        }
+        entity = readdir(dir);
+    }
+
+    closedir(dir);
+}*/
+
 int main(){
     int input;
     int end = 0;
@@ -29,11 +53,11 @@ int main(){
     char rm_file[BUF];
     char new_file[BUF];
     char initial_file[BUF];
-    char prev[BUF];
+    char prev[100];
     current[0] = 'C';
     printf("Starting in the C drive\n");
     dir = opendir(current);
-    int remove;
+    int found;
     int contains = 0;
     while(1){
         
@@ -41,10 +65,10 @@ int main(){
         scanf("%d",&input);
         fflush (stdin);
         switch(input){
-            case 0:  // prints current directory
+            case 0: 
                 printf("Current directory is %s\n", current);
                 break;
-            case 1: // prints all files in current directory
+            case 1:
                 printf("Displaying contents of folder\n");
                 entity = readdir(dir);
                 while(entity != NULL){
@@ -53,13 +77,21 @@ int main(){
                 }
                 printf("\n");
                 break;
-            case 2: // changes the current directory
-                closedir(dir);
+            case 2:
+                printf(""); //idk why but it needs this
                 char path[100] = { 0 };
-                strcpy(prev, current);
                 printf("enter name of folder\n");
                 gets(go_to);
                 fflush(stdin);
+                printf("%s\n",go_to);
+                if(!strcmp(go_to,"..")){
+                    closedir(dir);
+                    printf("going back\n");
+                    strcpy(current,prev);
+                    dir = opendir(prev);
+                    break;
+                }
+                strcpy(prev, current);
                 strcat(path,current);
                 strcat(path,go_to);
                 strcat(path,"/");
@@ -76,34 +108,73 @@ int main(){
                 }
                 break;
 
-            case 3: //deletes file in current directory
+            case 3: 
                 printf("enter name of folder you want to remove\n");
-                //scanf("%s", rm_file);
+                char path1[100] = { 0 };
                 gets(rm_file);
                 fflush(stdin);
-                remove = rmdir(rm_file);
-                printf("Remove is: %d\n", remove);
-                if (remove == 0)
-                {
-                    printf("Given empty directory removed successfully\n");
-                }    
-                else
-                {
-                    printf("Unable to remove directory %s\n", rm_file);
+                found = 0;
+                entity = readdir(dir);
+                while(entity != NULL){
+                    printf("%s\n",entity->d_name);
+                    if(!strcmp(entity->d_name,rm_file)){
+                        found = 1;
+                        printf("removing %s\n",rm_file);
+                        strcat(path1,current);
+                        strcat(path1,rm_file);
+                        strcat(path1,"/");
+                        if (rmdir(path1)==0)
+                        {
+                            printf("Given empty directory removed successfully\n");
+                        }    
+                        else
+                        {
+                            printf("Unable to remove directory %s\n", rm_file);
+                        }
+                    }
+                    entity = readdir(dir);
                 }
-                    
+                if(found == 0){
+                        printf("File not found\n");
+                }
+                closedir(dir);
+                dir = opendir(current);
                 /* use a for loop to check to see if the folder already exists
                    if it doesnt then print an error message and break
                    else delete the folder*/
                 break;
-            case 4: //creates a file in current directory
+            case 4: 
                 printf("enter name of folder you want to create\n");
-                scanf("%s", new_file);
+                char path2[100] = { 0 };
+                gets(rm_file);
+                fflush(stdin);
+                found = 0;
+                entity = readdir(dir);
+                while(entity != NULL){
+                    printf("%s\n",entity->d_name);
+                    if(!strcmp(entity->d_name,rm_file)){
+                        found = 1;
+                        printf("found %s, folder creation failed\n",rm_file);
+                    }
+                    entity = readdir(dir);
+                }
+                if(found == 0){
+                        printf("Double not found, Creating folder\n");
+                        strcat(path2,current);
+                        strcat(path2,rm_file);
+                        strcat(path2,"/");
+                        if(mkdir(path2)){
+                            printf("Succesful\n");
+                        }
+                }
+                
+                closedir(dir);
+                dir = opendir(current);
                 /* use a for loop to check to see if the folder already exists
                    if it does then print an error message and break
                    else create the folder*/
                 break;
-            case 5: //copies a file in the currrent directory
+            case 5:
                 printf("enter name of folder you want to copy\n");
                 //scanf("%s", initial_file);
                 gets(initial_file);
